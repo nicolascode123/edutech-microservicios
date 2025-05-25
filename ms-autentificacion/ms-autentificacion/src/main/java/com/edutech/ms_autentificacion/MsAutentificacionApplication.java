7247bbcb5bc4a66c48d3a8fe1661bda298869d70
nicolascode 123 <ni.fernandezv@duocuc.cl>
2025-05-25 17:17:04 -0400

    Subida completa de todos los microservicios

---
+++ ms-autentificacion/ms-autentificacion/src/main/java/com/edutech/ms_autentificacion/MsAutentificacionApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.ms_autentificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAutentificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAutentificacionApplication.class, args);
+	}
+
+}
---
+++ ms-notificaciones/ms-notificaciones/src/main/java/com/edutech/ms_notificaciones/NotificacionRepository.java
@@ -0,0 +1,6 @@
+package com.edutech.ms_notificaciones;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
+}
